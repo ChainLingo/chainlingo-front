2efeb4c04bb9aaad6350f74b4199b05cc0bba41c
Emi <emi.hokama.eckstein@gmail.com>
2023-08-20 21:26:04 +0900

    Add utility function

--- src/util/helper.ts
+++ src/util/helper.ts
@@ -1,1 +1,28 @@
-export const randomNum26 = () => { return Math.floor(Math.random() * 26) }
+export const getRandomChar = () => {
+    const alphabet = "aabcdeefghiijklmnoprstuvwyz";
+    const randomIndex = Math.floor(Math.random() * alphabet.length);
+    return alphabet[randomIndex];
+}
+
+export const getUniqueChar = (arr: string[]): string => {
+    const char = getRandomChar();
+    console.log(char)
+    if (!arr.includes(char)) return char;
+    else return getUniqueChar(arr)
+}
+
+export const updatePlayerTiles = (prevTiles: string[], index: number) => {
+    let copyArr = [...prevTiles]
+    const uniqueChar = getUniqueChar(prevTiles)
+    copyArr[index] = uniqueChar
+    return copyArr;
+}
+
+export const getFullPlayerTiles = () => {
+    const newTile = [getRandomChar()];
+    for (let i = 1; i <= 3; i++) {
+        newTile.push(getUniqueChar(newTile))
+    }
+    return newTile
+}
+
